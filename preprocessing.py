6903bdaed75a7f5ed13b30df1afe64d3fb4bf387
benjaminpjl <benjaminpujol@MacBook-Pro-de-Benjamin-3.local>
2017-02-19 18:39:25 +0100

    building TFIDF

--- preprocessing.py
+++ preprocessing.py
@@ -8,6 +8,7 @@ Created on Sun Feb 19 15:13:40 2017
 
 import pandas as pd
 import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def improve_info():
     
@@ -28,26 +29,25 @@ def improve_info():
         i=0
         
         #Looping over X_rain
-        for edge in X_train:
+         
+        for edge in X_train :
             if edge[2] == 1:
-                #Updating parents array
+                # Updating parents array
+
                 if parents[Index.loc[edge[1]][0]][0] == '':
-                    parents[Index.loc[edge[1]][0]] == str(edge[0])
-                else:
-                    parents[Index.loc[edge[1]][0]] == parents[Index.loc[edge[1]][0]] + " " + str(edge[0])
-                
-                    
+                    parents[Index.loc[edge[1]][0]] = str(edge[0])
+                else :
+                    parents[Index.loc[edge[1]][0]] = parents[Index.loc[edge[1]][0]] + " " + str(edge[0])
+
                 # Updating childs array
                 if childs[Index.loc[edge[0]][0]][0]   == '':
                     childs[Index.loc[edge[0]][0]]   = str(edge[1])
                 else :
                     childs[Index.loc[edge[0]][0]]   = childs[Index.loc[edge[0]][0]]   + " " + str(edge[1])
-
-            
-            if (i + 1) % int(len(X_train)/10) == 0:
+                    
+            if (i + 1) % int(len(X_train)/4) == 0:
                 print(str(i) + "/" + str(len(X_train)) + " samples processsed (" + str(100*i/len(X_train)) + "%)")
-            
-            i = i+1
+            i = i + 1
             
         #Improving node file info
         Inf = np.hstack([Inf, parents, childs])
@@ -56,4 +56,20 @@ def improve_info():
         print('Childs and parents graph have been generated and stored on disk')
 
 improve_info()
+
+def buildTDIDF():
+   
+    # File paths
+    tdidf_title = '/Users/benjaminpujol/Documents/Cours3A/CitationNetwork/tdidf_title.mtx'
+    tdidf_abstract = '/Users/benjaminpujol/Documents/Cours3A/CitationNetwork/tdidf_abstract.mtx'
+    
+    # Building term vectore representation of title and abstract
+    if os.path.isfile(tdidf_title) and os.path.isfile(tdidf_abstract):
+        return io.mmread(tdidf_title).tocsr(), io.mmread(tdidf_abstract).tocsr()
+    
+    elif os.path.isfile(tditf_title):
+        #Generating term vectorisation for abstract
+        Inf = pd.read_csv('/Users/benjaminpujol/Documents/Cours3A/CitationNetwork/node_information.csv', header = None).set_index(0).values
+        vec = TfidVectorizer(sublinear_tf = True, max_df = 0.5, stop_words = "english" )  #Apply sublinear tf scaling ie replace tf -> 1 + log(tf) and ignore words that appear in more than 50% of docs
+    
             
